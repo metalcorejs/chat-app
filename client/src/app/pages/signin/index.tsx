41eb27e2c3ee4d33848a467f35505f1f8e491af1
Tony Kark <tonyfullstackkark10@gmail.com>
2021-03-20 17:28:38 +0300

    signin form update

--- client/src/app/pages/signin/index.tsx
+++ client/src/app/pages/signin/index.tsx
@@ -1,3 +1,25 @@
+import { TextField } from "../../ui/text_field";
+
 export const SignIn = () => {
-  return <h1>Sign In</h1>;
+  return (
+    <form action='' className='signin'>
+      <h2 className='signin__title'>Account Registration</h2>
+      <TextField
+        type='text'
+        name='username'
+        id='username'
+        label='User name'
+        placeholder='name...'
+        className='signin__text-field'
+      />
+      <TextField
+        type='text'
+        name='userlastname'
+        id='userlastname'
+        label='User last name'
+        placeholder='last name...'
+        className='signin__text-field'
+      />
+    </form>
+  );
 };
